3f3ed42132c314addac66444391a65635237f536
guillermoLeon26 <gleon2611@gmail.com>
2019-01-30 23:56:17 -0500

    mejora

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var date = new Date();
-var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-//Math.floor((Math.random() * maxData) + minData);
-var value = Math.floor((Math.random() * 50) + 25).toString();
-
 const http = require('http');
-
-const jsonObject = JSON.stringify({
-  'equipo': '5c4f2aeb6aa0bb0620c11943',
-  'sensor': '5c51267c7712621e7c1abaaf',
-  'value': value,
-  'time': time
-});
 
 const options = {
   hostname: '127.0.0.1',
@@ -22,19 +10,33 @@ const options = {
   }
 };
 
-var req = http.request(options, function(res) {
-  console.log("statusCode: ", res.statusCode);
-  
-  res.on('data', function(d) {
+const envios = () => {
+  var req = http.request(options, function(res) {
+    console.log("statusCode: ", res.statusCode);
+    
+    res.on('data', function(d) {
       console.info('POST result:\n');
       process.stdout.write(d);
       console.info('\n\nPOST completed');
+    });
   });
-});
 
-// write the json data
-req.write(jsonObject);
-req.end();
-req.on('error', function(e) {
-    console.error(e);
-});
+  var date = new Date();
+  var time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+  //Math.floor((Math.random() * maxData - minData + 1) + minData);
+  var value = Math.floor((Math.random() * 25) + 25).toString();
+
+  const jsonObject = JSON.stringify({
+    'equipo': '5c4f2aeb6aa0bb0620c11943',
+    'sensor': '5c51267c7712621e7c1abaaf',
+    'value': value,
+    'time': time
+  });
+
+  req.write(jsonObject, 'utf8', (err) => {
+    if (err) console.error(e);
+    req.end();
+  });
+}
+
+setInterval(envios, 3000);
